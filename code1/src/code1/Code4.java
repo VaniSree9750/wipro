bd6dd23656d9ba971570f1e527a8760b53d67400
Hp <Hp@LAPTOP-9V4F54HF>
2025-07-28 20:38:34 +0530

    first commit

---
+++ code1/src/code1/Code4.java
@@ -0,0 +1,18 @@
+package code1;
+import java.util.Scanner;
+public class Code4 {
+ public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+ System.out.print("Enter first number: ");
+ int a = sc.nextInt();
+ System.out.print("Enter second number: ");
+ int b = sc.nextInt();
+ int max = (a > b) ? a : b;
+ System.out.println("Largest: " + max);
+ sc.close();
+ }
+}
+
+
+
+
